2c341a07db9e4eb8b9c0c6778d9feec9c3f6cff6
unkuseni <amudalatadeoye@gmail.com>
2023-08-26 23:08:52 +0100

    Learning about generics

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    println!("The largest number is {}", largest(&number_list));
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    println!("The largest char is {}", largest(&char_list));
+}
+// This is a test for finding the largest number in a list
+fn _largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+// This is a test for finding the largest char in a list
+fn _largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+// We can also see that the largest fn is also a duplicate code we can reduce this to one fn 
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
